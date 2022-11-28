31e4cae69bd3ef4fe2525d5fadc5da2f7d7604b4
yjyj1023 <lyj19961023@gmail.com>
2022-11-28 15:35:34 +0900

    테스트코드 작성

--- src/main/java/com/hospitalreview/domain/dto/UserJoinRequest.java
+++ src/main/java/com/hospitalreview/domain/dto/UserJoinRequest.java
@@ -2,12 +2,14 @@ package com.hospitalreview.domain.dto;
 
 import com.hospitalreview.domain.User;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Builder
 public class UserJoinRequest {
     private String userName;
     private String password;
---
+++ src/test/java/com/hospitalreview/controller/UserControllerTest.java
@@ -0,0 +1,73 @@
+package com.hospitalreview.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hospitalreview.domain.dto.UserDto;
+import com.hospitalreview.domain.dto.UserJoinRequest;
+import com.hospitalreview.exception.ErrorCode;
+import com.hospitalreview.exception.HospitalReviewAppException;
+import com.hospitalreview.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void join_success() throws Exception {
+        UserJoinRequest userJoinRequest = UserJoinRequest.builder()
+                .userName("kyeongrok")
+                .password("1q2w3e4r")
+                .email("oceanfog1@gmail.com")
+                .build();
+
+        when(userService.join(any())).thenReturn(mock(UserDto.class));
+
+        mockMvc.perform(post("/api/v1/users/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패")
+    void join_fail() throws Exception {
+
+        UserJoinRequest userJoinRequest = UserJoinRequest.builder()
+                .userName("kyeongrok")
+                .password("1q2w3e4r")
+                .email("oceanfog1@gmail.com")
+                .build();
+
+        when(userService.join(any())).thenThrow(new HospitalReviewAppException(ErrorCode.DUPLICATED_USER_NAME, ""));
+
+        mockMvc.perform(post("/api/v1/users/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andDo(print())
+                .andExpect(status().isConflict());
+    }
+}
